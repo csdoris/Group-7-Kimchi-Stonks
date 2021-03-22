ee35b56dffea9f249a32952a74fa8240df3ea3dc
Andrew <afei088@aucklanduni.ac.nz>
2021-03-23 08:29:38 +1300

    added PORT as an environment variable

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = 8080;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server currently listening to http://localhost:${port}`);
