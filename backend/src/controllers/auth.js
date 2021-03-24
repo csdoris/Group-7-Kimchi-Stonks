1185cc0b129a76cb6d20fa9e4eabacf56bfadaa5
Andrew <afei088@aucklanduni.ac.nz>
2021-03-25 12:52:50 +1300

    added comments for auth register controller and service functions

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,7 +1,15 @@
 const AuthService = require('../services/auth');
 
+/**
+ * Extracts the request body and uses it to create a new user in the database.
+ *
+ * @param  {Object} req Request object
+ * @param  {Object} res Response object
+ */
 async function registerNewUser(req, res) {
-  const { firstName, lastName, email, password } = req.body;
+  const {
+    firstName, lastName, email, password,
+  } = req.body;
 
   const { status, json } = await AuthService.createNewUser(firstName, lastName, email, password);
 
--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -1,7 +1,18 @@
 const User = require('../mongodb/schemas/userSchema');
 
+/**
+ * Creates a new user using the user schema and persists it to the database.
+ *
+ * @param  {String} firstName User's first name
+ * @param  {String} lastName  User's last name
+ * @param  {String} email     User's email
+ * @param  {String} password  User's password
+ * @return {Object}           Object containing a status and json response property
+ */
 async function createNewUser(firstName, lastName, email, password) {
-  const newUser = new User({ firstName, lastName, email, password });
+  const newUser = new User({
+    firstName, lastName, email, password,
+  });
   const { err } = await newUser.save();
 
   if (err) {
