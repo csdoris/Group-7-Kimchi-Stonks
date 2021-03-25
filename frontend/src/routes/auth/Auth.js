71824f53e9a0215e7c9f8bbefd57abfde8974b96
Andrew <afei088@aucklanduni.ac.nz>
2021-03-25 14:52:16 +1300

    added nested router for login and register forms in auth route

--- frontend/src/routes/auth/Auth.js
+++ frontend/src/routes/auth/Auth.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
 
+import LoginForm from './LoginForm/LoginForm';
 import RegisterForm from './RegisterForm/RegisterForm';
 
 function Auth() {
+  const { path } = useRouteMatch();
+
   return (
     <div>
-      <div>
-        <RegisterForm />
-      </div>
+      <Switch>
+        <Route path={`${path}/login`}>
+          <LoginForm />
+        </Route>
+        <Route path={`${path}/register`}>
+          <RegisterForm />
+        </Route>
+        <Route path={path}}>
+          <Redirect to={`${path}/login`} />
+        </Route>
+      </Switch>
     </div>
   );
 }
