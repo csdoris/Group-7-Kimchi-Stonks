b21ccf86d294e68d40e30100b1c94e6677776f1d
Andrew <afei088@aucklanduni.ac.nz>
2021-03-25 19:27:55 +1300

    added styling for register form

--- frontend/src/routes/Auth/RegisterForm/RegisterForm.js
+++ frontend/src/routes/Auth/RegisterForm/RegisterForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import InputField from '../../../components/InputField/InputField';
 import Button from '../../../components/Button/Button';
+
+import logo from '../../../assets/logo.png';
 
 function RegisterForm() {
   const [firstName, setFirstName] = useState('');
@@ -12,49 +15,58 @@ function RegisterForm() {
 
   return (
     <form className="auth-form">
-      <InputField
-        type="text"
-        name="first-name"
-        value={firstName}
-        placeholder="First Name"
-        onChange={(event) => setFirstName(event.target.value)}
-      />
-      <InputField
-        type="text"
-        name="last-name"
-        value={lastName}
-        placeholder="Last Name"
-        onChange={(event) => setLastName(event.target.value)}
-      />
-      <InputField
-        type="text"
-        name="email"
-        value={email}
-        placeholder="Email"
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <InputField
-        type="password"
-        name="password"
-        value={password}
-        placeholder="Password"
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <InputField
-        type="password"
-        name="confirm-password"
-        value={confirmPassword}
-        placeholder="Confirm Password"
-        onChange={(event) => setConfirmPassword(event.target.value)}
-      />
-      <Button
-        className="submit"
-        type="submit"
-        value="Submit"
-        text="Register"
-        variant="contained"
-        onClick={() => console.log('Submit Button Clicked!')}
-      />
+      <div className="form-content">
+        <img className="logo" src={logo} alt="Kimchi Stonks Logo" />
+        <InputField
+          type="text"
+          name="first-name"
+          value={firstName}
+          placeholder="First Name"
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+        <InputField
+          type="text"
+          name="last-name"
+          value={lastName}
+          placeholder="Last Name"
+          onChange={(event) => setLastName(event.target.value)}
+        />
+        <InputField
+          type="text"
+          name="email"
+          value={email}
+          placeholder="Email"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <InputField
+          type="password"
+          name="password"
+          value={password}
+          placeholder="Password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <InputField
+          type="password"
+          name="confirm-password"
+          value={confirmPassword}
+          placeholder="Confirm Password"
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+        <Button
+          className="submit"
+          type="submit"
+          value="Submit"
+          text="Register"
+          variant="contained"
+          onClick={() => console.log('Submit Button Clicked!')}
+        />
+      </div>
+      <div className="form-footer">
+        <p>Already have an account?</p>
+        <Link className="button text" to="/auth/login">
+          Login here
+        </Link>
+      </div>
     </form>
   );
 }
