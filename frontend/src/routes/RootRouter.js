e75a463616bb21de0039df332ff51b5e9c8eae04
Andrew <afei088@aucklanduni.ac.nz>
2021-03-25 14:00:19 +1300

    created root router component

---
+++ frontend/src/routes/RootRouter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import Auth from './auth/Auth';
+import Dashboard from './dashboard/Dashboard';
+
+function RootRouter() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/dashboard">
+          <Redirect to="/dashboard" />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default RootRouter;
