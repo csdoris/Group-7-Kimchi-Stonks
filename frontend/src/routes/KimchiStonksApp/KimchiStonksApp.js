d87385ef4e4b117d2720802d8ab327a1594e5127
Andrew <afei088@aucklanduni.ac.nz>
2021-03-26 14:18:49 +1300

    create kimchi stonks app component

---
+++ frontend/src/routes/KimchiStonksApp/KimchiStonksApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function KimchiStonksApp() {
+  return (
+    <div>
+      <div>
+        KimchiStonksApp
+      </div>
+    </div>
+  );
+}
+
+export default KimchiStonksApp;
