6199f65f4668a96d360e438766535be2ee9364dc
afei088 <60560589+afei088@users.noreply.github.com>
2021-03-26 17:05:12 +1300

    [FEAT] Add market and stocks routes in FRONTEND (#49)
    * added market component
    
    * added stock component
    
    * added navbar component
    
    * create kimchi stonks app component
    
    * fixed routing for dashboard, market and stock routes
    
    * added user info bar component
    
    * using user info bar in application

---
+++ frontend/src/routes/KimchiStonksApp/KimchiStonksApp.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Switch, Route, Redirect,
+} from 'react-router-dom';
+
+import NavBar from './NavBar/NavBar';
+import UserInfoBar from './UserInfoBar/UserInfoBar';
+import Stock from './Stock/Stock';
+import Dashboard from './Dashboard/Dashboard';
+import Market from './Market/Market';
+
+function KimchiStonksApp() {
+  return (
+    <div>
+      <NavBar />
+      <UserInfoBar />
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/market">
+          <Market />
+        </Route>
+        <Route path="/stock">
+          <Stock />
+        </Route>
+        <Route path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default KimchiStonksApp;
