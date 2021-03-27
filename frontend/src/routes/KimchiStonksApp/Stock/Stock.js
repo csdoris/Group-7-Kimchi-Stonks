4241c4bea66af15d1e96fd484fb55b3a20900e55
afei088 <60560589+afei088@users.noreply.github.com>
2021-03-28 12:16:36 +1300

    [FEAT] Add stock route layout in FRONTEND (#67)
    - added grid styling for stock route

--- frontend/src/routes/KimchiStonksApp/Stock/Stock.js
+++ frontend/src/routes/KimchiStonksApp/Stock/Stock.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import './Stock.css';
+
 function Stock() {
   return (
-    <div>
-      <div>
-        Stock Route
+    <div className="stock-container">
+      <div className="left-container">
+        <div className="graph-container">
+          <p>Top</p>
+        </div>
+        <div className="overview-container">
+          <p>Bottom</p>
+        </div>
+      </div>
+      <div className="right-container">
+        <p>Right</p>
       </div>
     </div>
   );
