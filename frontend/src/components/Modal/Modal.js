abfd5aa576d9bb392d1cbc700c7b670ea6a8d94e
afei088 <60560589+afei088@users.noreply.github.com>
2021-03-28 13:07:50 +1300

    [FEAT] Add modal dialog component for prompting user input in FRONTEND (#71)
    * created modal component using portal
    
    * updated eslint rules

---
+++ frontend/src/components/Modal/Modal.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useHistory } from 'react-router-dom';
+
+import './Modal.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+function Modal({
+  dismissOnClickOutside, className, children,
+}) {
+  const history = useHistory();
+
+  function handleModalBackground(event) {
+    if (dismissOnClickOutside && event.target.parentElement === modalRoot) {
+      history.goBack();
+    }
+  }
+
+  return ReactDOM.createPortal(
+    <div
+      className="modal-background"
+      onClick={(event) => handleModalBackground(event)}
+    >
+      <div className={`modal ${className}`}>
+        {children}
+      </div>
+    </div>,
+    modalRoot,
+  );
+}
+
+export default Modal;
