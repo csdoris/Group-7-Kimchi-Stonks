47accf0aa978e248263ddf718c49792a7c9d0739
Ben Chen <55371154+bche536@users.noreply.github.com>
2021-03-30 10:57:16 +1300

    [MISC] Refactor TableRow and TableHeader  (#83)
    * Refactored headers and rows into single component, added conditional styling for positive/negative changes
    
    * Refactored headers and rows into single component, added conditional styling for positive/negative changes

---
+++ frontend/src/components/Table/Table.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './Table.scss';
+
+function TableHeader() {
+  const titles = [
+    'STOCK',
+    'SHARES',
+    'AVG PRICE',
+    'VALUE',
+    'DAY CHANGE',
+    'TOTAL CHANGE',
+    'ACTION',
+  ];
+
+  const holdingsList = [
+    {
+      stock: 'AMD',
+      shares: '1.00',
+      avgPrice: '$95.50',
+      value: '$76.27',
+      dayChange: '-$0.26 (-0.34%)',
+      totalChange: '-$18.99 (-19.93%)',
+      action: 'SELL',
+    },
+    {
+      stock: 'DBX',
+      shares: '1.00',
+      avgPrice: '$95.50',
+      value: '$76.27',
+      dayChange: '+$0.26 (-0.34%)',
+      totalChange: '+$18.99 (-19.93%)',
+      action: 'SELL',
+    },
+    {
+      stock: 'TSLA',
+      shares: '1.00',
+      avgPrice: '$595.50',
+      value: '$76.27',
+      dayChange: '+$0.26 (-0.34%)',
+      totalChange: '+$18.99 (-19.93%)',
+      action: 'SELL',
+    },
+  ];
+
+  return (
+    <div className="table">
+      <div className="table-header flex-grid">
+        {titles.map((title) => (
+          <div className="col">{title}</div>
+        ))}
+      </div>
+      {holdingsList.map((holding) => (
+        <div className="table-row flex-grid">
+          {Object.keys(holding).map((keyName) => (
+            <div
+              className={`col ${keyName} ${
+                holding[keyName][0] === '+' ? 'positive' : ''
+              } 
+              ${holding[keyName][0] === '-' ? 'negative' : ''}`}
+            >
+              {holding[keyName]}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TableHeader;
--- frontend/src/routes/KimchiStonksApp/Dashboard/Dashboard.js
+++ frontend/src/routes/KimchiStonksApp/Dashboard/Dashboard.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-import TableHeader from '../TableHeader/TableHeader';
-import TableRow from '../TableRow/TableRow';
-import TITLES from './DashboardDummyTitles';
-import DUMMY_HOLDINGS_LIST from './DashboardDummyHoldingsList';
+import Table from '../../../components/Table/Table';
 
 import './Dashboard.scss';
 
@@ -11,12 +8,7 @@ function Dashboard() {
   return (
     <div className="dashboard-container">
       <h1 className="dashboard-title">HOLDINGS</h1>
-      <div className="table-container">
-        <TableHeader titles={TITLES} />
-        {DUMMY_HOLDINGS_LIST.map((holding) => (
-          <TableRow className="col" holding={holding} />
-        ))}
-      </div>
+      <Table />
     </div>
   );
 }
