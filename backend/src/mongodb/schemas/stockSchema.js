86fa5955bde8d1d637f6e87e338e96be992a163d
yungikim1999 <danielkim1201@gmail.com>
2021-03-31 09:24:49 +1300

    stock schema added

---
+++ backend/src/mongodb/schemas/stockSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const stockSchema = mongoose.Schema({
+  stockName: {
+    type: String,
+    required: true,
+  },
+  stockSymbol: {
+    type: String,
+    required: true,
+  },
+  totalShares: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  averagePrice: {
+    type: Number,
+    required: true,
+  },
+  owner: { type: Schema.Types.ObjectId, ref: 'User' },
+});
+
+const Stock = mongoose.model('Stock', stockSchema);
+
+module.exports = Stock;
