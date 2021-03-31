69fb358db518fb0dd403a56e7ece07a5538b8de9
Andrew <afei088@aucklanduni.ac.nz>
2021-03-31 18:10:03 +1300

    storing info in local storage working

--- frontend/src/contexts/Auth.js
+++ frontend/src/contexts/Auth.js
@@ -7,6 +7,32 @@ const url = process.env.REACT_APP_API_URL;
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(undefined);
+  const [token, setToken] = useState(undefined);
+
+  function saveAuthData(token, expirationDate, userId) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+    localStorage.setItem('userId', userId);
+  }
+
+  function clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+    localStorage.removeItem('userId');
+  }
+
+  function logOut() {
+    // TODO: Remove local storage
+    setUser(undefined);
+    clearAuthData();
+    console.log(token);
+  }
+
+  function setAuthTimer(duration) {
+    setTimeout(() => {
+      logOut();
+    }, duration * 1000);
+  }
 
   function register(firstName, lastName, email, password) {
     axios.post(`${url}/auth/register`, {
@@ -33,18 +59,23 @@ function AuthProvider({ children }) {
     }).then((res) => {
       const { status, data } = res;
       const { user } = data;
+      const { token, expiresIn } = user.accessToken;
 
-      if (status === 200) {
+      if (status === 200 && token) {
+        setToken(token);
+        setAuthTimer(5);
         setUser(user);
+
+        const now = new Date();
+        const expirationDate = new Date(now.getTime() + expiresIn * 1000);
+
+        saveAuthData(token, expirationDate, user._id);
+
+        console.log(expiresIn);
       }
 
       // TODO: Error Message
     });
-  }
-
-  function logOut() {
-    // TODO: Remove local storage
-    setUser(undefined);
   }
 
   const context = {
