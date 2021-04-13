1d9d19707555c9dd0ee7d8d5e880fecc49fa2e92
Kyle Zhou <49678883+kyzh0@users.noreply.github.com>
2021-04-13 16:41:47 +1200

    [FEAT] Add Dashboard Endpoints in BACKEND (#98)
    * implement endpont + fix linting rules
    
    * rename resp variable

--- backend/src/controllers/dashboard.js
+++ backend/src/controllers/dashboard.js
@@ -63,21 +63,37 @@ async function getTimeSeriesMonthly(req, res) {
  */
 async function getTrending(req, res) {
   const url = `https://financialmodelingprep.com/api/v3/stock/gainers?apikey=${process.env.FMP_API_KEY}`;
-  axios.get(url).then((resp) => {
-    res.status(resp.status).json(resp.data);
+  axios.get(url).then((response) => {
+    res.status(response.status).json(response.data);
   }).catch((err) => {
     res.status(err.response.status).json(err.response.data);
   });
 }
 
 /**
- * Calculates a price prediction for the stock symbol passed as the path param.
+ * Uses the analyst price target as a prediction for the stock price in 12 months
+ * for the symbol passed as the path param. Optional query param 'days' to
+ * calculate the predicted price for x days in the future.
  *
  * @param  {Object} req Request object
  * @param  {Object} res Response object
  */
 async function predictPrice(req, res) {
-  res.json(req);
+  const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${req.params.id}&apikey=${process.env.AV_API_KEY}`;
+  axios.get(url).then((response) => {
+    const days = parseInt(req.query.days, 10);
+    const currentPrice = parseFloat(response.data.MarketCapitalization) / parseFloat(response.data.SharesOutstanding);
+    let prediction = response.data.AnalystTargetPrice;
+
+    if (days >= 0) {
+      prediction = currentPrice + (prediction - currentPrice) * days / 365;
+    }
+
+    const predictionJSON = { prediction: Math.round(prediction * 100) / 100 };
+    res.status(response.status).json(predictionJSON);
+  }).catch((err) => {
+    res.status(err.response.status).json(err.response.data);
+  });
 }
 
 module.exports = {
