eda4ad84a6430dd2bc861ec42f43d6fc36317013
Andrew <afei088@aucklanduni.ac.nz>
2021-04-21 09:50:03 +1200

    added validate user middleware to dashboard routes

--- backend/src/routes/dashboard.js
+++ backend/src/routes/dashboard.js
@@ -1,21 +1,22 @@
 const express = require('express');
 
+const validateUser = require('../middleware/validateUser');
 const DashboardController = require('../controllers/dashboard');
 
 const router = express.Router();
 
-router.get('/stock-overview/:id', DashboardController.getStockOverview);
+router.get('/stock-overview/:id', validateUser, DashboardController.getStockOverview);
 
-router.get('/time-series/intraday/:id', DashboardController.getTimeSeriesIntraday);
+router.get('/time-series/intraday/:id', validateUser, DashboardController.getTimeSeriesIntraday);
 
-router.get('/time-series/daily/:id', DashboardController.getTimeSeriesDaily);
+router.get('/time-series/daily/:id', validateUser, DashboardController.getTimeSeriesDaily);
 
-router.get('/time-series/weekly/:id', DashboardController.getTimeSeriesWeekly);
+router.get('/time-series/weekly/:id', validateUser, DashboardController.getTimeSeriesWeekly);
 
-router.get('/time-series/monthly/:id', DashboardController.getTimeSeriesMonthly);
+router.get('/time-series/monthly/:id', validateUser, DashboardController.getTimeSeriesMonthly);
 
-router.get('/trending-stocks', DashboardController.getTrending);
+router.get('/trending-stocks', validateUser, DashboardController.getTrending);
 
-router.get('/predict-price/:id', DashboardController.predictPrice);
+router.get('/predict-price/:id', validateUser, DashboardController.predictPrice);
 
 module.exports = router;
