858f92cd77421f0c729be4495a8936ffc1eeb416
Andrew <afei088@aucklanduni.ac.nz>
2021-04-21 09:51:09 +1200

    changed path parameter id to symbol

--- backend/src/routes/dashboard.js
+++ backend/src/routes/dashboard.js
@@ -5,18 +5,18 @@ const DashboardController = require('../controllers/dashboard');
 
 const router = express.Router();
 
-router.get('/stock-overview/:id', validateUser, DashboardController.getStockOverview);
+router.get('/stock-overview/:symbol', validateUser, DashboardController.getStockOverview);
 
-router.get('/time-series/intraday/:id', validateUser, DashboardController.getTimeSeriesIntraday);
+router.get('/time-series/intraday/:symbol', validateUser, DashboardController.getTimeSeriesIntraday);
 
-router.get('/time-series/daily/:id', validateUser, DashboardController.getTimeSeriesDaily);
+router.get('/time-series/daily/:symbol', validateUser, DashboardController.getTimeSeriesDaily);
 
-router.get('/time-series/weekly/:id', validateUser, DashboardController.getTimeSeriesWeekly);
+router.get('/time-series/weekly/:symbol', validateUser, DashboardController.getTimeSeriesWeekly);
 
-router.get('/time-series/monthly/:id', validateUser, DashboardController.getTimeSeriesMonthly);
+router.get('/time-series/monthly/:symbol', validateUser, DashboardController.getTimeSeriesMonthly);
 
 router.get('/trending-stocks', validateUser, DashboardController.getTrending);
 
-router.get('/predict-price/:id', validateUser, DashboardController.predictPrice);
+router.get('/predict-price/:symbol', validateUser, DashboardController.predictPrice);
 
 module.exports = router;
