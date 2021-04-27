5fcde7daaf00990a99b3d9f73b5e76d2ae7d63a0
Andrew <afei088@aucklanduni.ac.nz>
2021-04-27 14:13:47 +1200

    updated holdings dashboard

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -5,29 +5,33 @@ function TableHeader({ headers, data }) {
   function handleClick() {}
 
   return (
-    <div className="table">
-      <div className="table-header flex-grid">
-        {headers.map((header) => (
-          <div className="col" key={header}>{header}</div>
-        ))}
-      </div>
-      {data.map((el) => (
-        <div className="table-row flex-grid" key={el.stock}>
-          {Object.keys(el).map((keyName) => (
-            <div
-              className={`col ${keyName} ${
-                el[keyName][0] === '+' ? 'positive' : ''
-              }
-              ${el[keyName][0] === '-' ? 'negative' : ''}`}
-              key={`${el.stock}-${keyName}`}
-              onClick={handleClick}
-            >
-              {el[keyName]}
+    data.length > 0
+      ? (
+        <div className="table">
+          <div className="header-container flex-grid">
+            {headers.map((header) => (
+              <div className="header" key={header}>{header}</div>
+            ))}
+          </div>
+          {data.map((el) => (
+            <div className="table-row flex-grid" key={el.stock}>
+              {Object.keys(el).map((keyName) => (
+                <div
+                  className={`table-value ${keyName} ${
+                    el[keyName][0] === '+' ? 'positive' : ''
+                  }
+                  ${el[keyName][0] === '-' ? 'negative' : ''}`}
+                  key={`${el.stock}-${keyName}`}
+                  onClick={handleClick}
+                >
+                  {el[keyName]}
+                </div>
+              ))}
             </div>
           ))}
         </div>
-      ))}
-    </div>
+      )
+      : <p>No Data</p>
   );
 }
 
--- frontend/src/routes/KimchiStonksApp/Dashboard/Dashboard.js
+++ frontend/src/routes/KimchiStonksApp/Dashboard/Dashboard.js
@@ -1,54 +1,59 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Table from '../../../components/Table/Table';
+import { AuthContext } from '../../../contexts/Auth';
 
 import './Dashboard.scss';
 
 function Dashboard() {
+  const { user } = useContext(AuthContext);
+
+  console.log(user);
+
   const headers = [
-    'STOCK',
-    'SHARES',
-    'AVG PRICE',
-    'VALUE',
-    'DAY CHANGE',
-    'TOTAL CHANGE',
-    'ACTION',
+    'Stock',
+    'Shares',
+    'Avg. Price',
+    'Value',
+    'Day Change',
+    'Total Change',
+    '',
   ];
 
-  const data = [
-    {
-      stock: 'AMD',
-      shares: '1.00',
-      avgPrice: '$95.50',
-      value: '$76.27',
-      dayChange: '-$0.26 (-0.34%)',
-      totalChange: '-$18.99 (-19.93%)',
-      action: 'SELL',
-    },
-    {
-      stock: 'DBX',
-      shares: '1.00',
-      avgPrice: '$95.50',
-      value: '$76.27',
-      dayChange: '+$0.26 (-0.34%)',
-      totalChange: '+$18.99 (-19.93%)',
-      action: 'SELL',
-    },
-    {
-      stock: 'TSLA',
-      shares: '1.00',
-      avgPrice: '$595.50',
-      value: '$76.27',
-      dayChange: '+$0.26 (-0.34%)',
-      totalChange: '+$18.99 (-19.93%)',
-      action: 'SELL',
-    },
-  ];
+  // const data = [
+  //   {
+  //     stock: 'AMD',
+  //     shares: '1.00',
+  //     avgPrice: '$95.50',
+  //     value: '$76.27',
+  //     dayChange: '-$0.26 (-0.34%)',
+  //     totalChange: '-$18.99 (-19.93%)',
+  //     action: 'SELL',
+  //   },
+  //   {
+  //     stock: 'DBX',
+  //     shares: '1.00',
+  //     avgPrice: '$95.50',
+  //     value: '$76.27',
+  //     dayChange: '+$0.26 (-0.34%)',
+  //     totalChange: '+$18.99 (-19.93%)',
+  //     action: 'SELL',
+  //   },
+  //   {
+  //     stock: 'TSLA',
+  //     shares: '1.00',
+  //     avgPrice: '$595.50',
+  //     value: '$76.27',
+  //     dayChange: '+$0.26 (-0.34%)',
+  //     totalChange: '+$18.99 (-19.93%)',
+  //     action: 'SELL',
+  //   },
+  // ];
 
   return (
     <div className="dashboard-container">
       <h1 className="dashboard-title">Holdings</h1>
-      <Table headers={headers} data={data} />
+      <Table headers={headers} data={user.stocks} />
     </div>
   );
 }
