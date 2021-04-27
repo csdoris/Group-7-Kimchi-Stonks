805c1ce638b082fd0e301932634ee4211f3958ef
Andrew <afei088@aucklanduni.ac.nz>
2021-04-27 17:49:21 +1200

    updated auth route repsonses

--- frontend/src/routes/KimchiStonksApp/Dashboard/Dashboard.js
+++ frontend/src/routes/KimchiStonksApp/Dashboard/Dashboard.js
@@ -59,25 +59,29 @@ function Dashboard() {
   return (
     <div className="dashboard-container">
       <h1 className="dashboard-title">Holdings</h1>
-      <Table>
-        <TableHeader
-          headers={headers}
-        />
-        {user.stocks.map((stock) => (
-          <TableRow
-            key={`holdings-${stock.symbol}`}
-            rowData={{
-              symbol: stock.symbol,
-              shares: stock.shares.toFixed(2),
-              averagePrice: stock.averagePrice.toFixed(2),
-              value: parseFloat(stock.shares * stock.averagePrice).toFixed(2),
-              totalChange: '+135.12 (+0.25%)',
-              action: 'sell',
-            }}
-            onClick={() => history.push(`/stock/${stock.symbol}?period=day`)}
-          />
-        ))}
-      </Table>
+      { user.stocks.length > 0
+        ? (
+          <Table>
+            <TableHeader
+              headers={headers}
+            />
+            {user.stocks.map((stock) => (
+              <TableRow
+                key={`holdings-${stock.symbol}`}
+                rowData={{
+                  symbol: stock.symbol,
+                  shares: stock.shares.toFixed(2),
+                  averagePrice: stock.averagePrice.toFixed(2),
+                  value: parseFloat(stock.shares * stock.averagePrice).toFixed(2),
+                  totalChange: '+135.12 (+0.25%)',
+                  action: 'sell',
+                }}
+                onClick={() => history.push(`/stock/${stock.symbol}?period=day`)}
+              />
+            ))}
+          </Table>
+        )
+        : <p>No Holdings :(</p>}
     </div>
   );
 }
